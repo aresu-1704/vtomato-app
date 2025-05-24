f8c72946d39b52012973a2c0a37d257ea1a1dbf2
Lý Thuận An <aresu.work1704@gmail.com>
2025-05-25 00:02:41 +0700

    Change email and config email in .env

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    EMAIL_SENDER: str
+    EMAIL_HOST: str
+    EMAIL_PORT: int
+    EMAIL_USERNAME: str
+    EMAIL_PASSWORD: str
 
     class Config:
         env_file = ".env"
